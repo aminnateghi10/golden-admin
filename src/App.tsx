bc8f5bfa8044f2643d54f81258e8b12fdb25f6a9
Hamid.J.Rahnama <hamidrahnamapc@gmail.com>
2023-12-09 17:36:12 +0330

    change costs utl to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React, {useEffect, Suspense, lazy} from 'react';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {ToastContainer} from 'react-toastify';
-import {Provider} from 'react-redux'
-import {store} from "./store";
+import React, { useEffect, Suspense, lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 //styles
-import 'react-toastify/dist/ReactToastify.css';
-import './style/codebase.css'
-import './style/main.css'
-import './style/rtl.css'
+import "react-toastify/dist/ReactToastify.css";
+import "./style/codebase.css";
+import "./style/main.css";
+import "./style/rtl.css";
 import EditCost from "./pages/edit-cost";
 import Payment from "./pages/payments";
 import EditPayment from "./pages/edit-payment";
 import AddUser from "./pages/add-user";
 
 //layouts
-const UserPanelLayout = lazy(() => import('./components/Layout/userPanelLayout'));
+const UserPanelLayout = lazy(
+  () => import("./components/Layout/userPanelLayout")
+);
 const UserAuthLayout = lazy(() => import("./components/Layout/userAuthLayout"));
 
 //pages
 const Dashboard = lazy(() => import("./pages"));
-const Login = lazy(() => import('./pages/auth/login'));
+const Login = lazy(() => import("./pages/auth/login"));
 const Costs = lazy(() => import("./pages/costs"));
 const Users = lazy(() => import("./pages/users"));
 const ChangePassword = lazy(() => import("./pages/change-password"));
@@ -28,51 +30,50 @@ const CreateCosts = lazy(() => import("./pages/create-costs"));
 const CreatePayments = lazy(() => import("./pages/create-payments"));
 
 const App = () => {
-
-    return (
-        <div className="App">
-            <Provider store={store}>
-                <BrowserRouter>
-                    <Suspense fallback={<h3>loading...</h3>}>
-                        <Routes>
-                            //user panel
-                            <Route path='/' element={<UserPanelLayout/>}>
-                                <Route path='/' element={<Dashboard/>}/>
-                                <Route path='/costs' element={<Costs/>}/>
-                                <Route path='/costs/:id' element={<EditCost/>}/>
-                                <Route path='/create-costs' element={<CreateCosts/>}/>
-                                <Route path='/users' element={<Users/>}/>
-                                <Route path='/change-password' element={<ChangePassword/>}/>
-                                <Route path='/create-payments' element={<CreatePayments/>}/>
-                                <Route path='/payments' element={<Payment/>}/>
-                                <Route path='/edit-payment' element={<EditPayment/>}/>
-                                <Route path='/add-user' element={<AddUser/>}/>
-                                <Route path='*' element={<>404</>}/>
-                            </Route>
-                            //auth
-                            <Route path='auth/' element={<UserAuthLayout/>}>
-                                <Route path='login' element={<Login/>}/>
-                            </Route>
-                            //not defind
-                            <Route path='*' element={<>404</>}/>
-                        </Routes>
-                    </Suspense>
-                </BrowserRouter>
-                <ToastContainer
-                    position="bottom-right"
-                    autoClose={5000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    theme="light"
-                />
-            </Provider>
-        </div>
-    );
-}
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <BrowserRouter>
+          <Suspense fallback={<h3>loading...</h3>}>
+            <Routes>
+              //user panel
+              <Route path="/" element={<UserPanelLayout />}>
+                {/* <Route path='/' element={<Dashboard/>}/> */}
+                <Route path="/" element={<Costs />} />
+                <Route path="/costs/:id" element={<EditCost />} />
+                <Route path="/create-costs" element={<CreateCosts />} />
+                <Route path="/users" element={<Users />} />
+                <Route path="/change-password" element={<ChangePassword />} />
+                <Route path="/create-payments" element={<CreatePayments />} />
+                <Route path="/payments" element={<Payment />} />
+                <Route path="/edit-payment" element={<EditPayment />} />
+                <Route path="/add-user" element={<AddUser />} />
+                <Route path="*" element={<>404</>} />
+              </Route>
+              //auth
+              <Route path="auth/" element={<UserAuthLayout />}>
+                <Route path="login" element={<Login />} />
+              </Route>
+              //not defind
+              <Route path="*" element={<>404</>} />
+            </Routes>
+          </Suspense>
+        </BrowserRouter>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </Provider>
+    </div>
+  );
+};
 
 export default App;
--- src/components/user-panel/sidebar.tsx
+++ src/components/user-panel/sidebar.tsx
@@ -1,70 +1,88 @@
-import React, {Dispatch, SetStateAction, memo ,useState} from "react";
-import {Link, NavLink} from "react-router-dom";
+import React, { Dispatch, SetStateAction, memo, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 interface PropsInterface {
-    sidebarShow:Dispatch<SetStateAction<boolean>>,
+  sidebarShow: Dispatch<SetStateAction<boolean>>;
 }
 
-const Sidebar = ({sidebarShow}:PropsInterface) => {
-    const sideNavigation =[
-        {href:'/' , icon:'si si-cup',label:'داشبورد'},
-        {href:'/create-costs', icon:'si si-plus',label:'ثبت هزینه'},
-        {href:'/costs' , icon:'si si-list',label:'لیست هزینه ها'},
-        {href:'/create-payments' , icon:'si si-credit-card',label:'ثبت پرداختی'},
-        {href:'/payments' , icon:'si si-book-open',label:'لیست پرداختی'},
-        {href:'/users' , icon:'si si-users',label:'لیست کاربران'},
-        {href:'/add-user' , icon:'si si-user-follow',label:'افزودن کاربر جدید'},
-    ];
+const Sidebar = ({ sidebarShow }: PropsInterface) => {
+  const sideNavigation = [
+    // {href:'/' , icon:'si si-cup',label:'داشبورد'},
+    { href: "/create-costs", icon: "si si-plus", label: "ثبت هزینه" },
+    { href: "/", icon: "si si-list", label: "لیست هزینه ها" },
+    {
+      href: "/create-payments",
+      icon: "si si-credit-card",
+      label: "ثبت پرداختی",
+    },
+    { href: "/payments", icon: "si si-book-open", label: "لیست پرداختی" },
+    { href: "/users", icon: "si si-users", label: "لیست کاربران" },
+    {
+      href: "/add-user",
+      icon: "si si-user-follow",
+      label: "افزودن کاربر جدید",
+    },
+  ];
 
-    return(
-      <nav id="sidebar">
-          {/* Sidebar Scroll Container */}
-          <div className="slimScrollDiv"><div id="sidebar-scroll">
-              {/* Sidebar Content */}
-              <div className="sidebar-content">
-                  {/* Side Header */}
-                  <div className="content-header content-header-fullrow px-15">
-                      {/* Normal Mode */}
-                      <div className="content-header-section text-center align-parent sidebar-mini-hidden">
-                          {/* Close Sidebar, Visible only on mobile screens */}
-                          {/* Layout API, functionality initialized in Codebase() -> uiApiLayout() */}
-                          <button onClick={()=>sidebarShow(false)} type="button" className="btn btn-circle btn-dual-secondary d-lg-none align-v-l" data-toggle="layout" data-action="sidebar_close">
-                              <i className="fa fa-times text-danger" />
-                          </button>
-                          {/* END Close Sidebar */}
-                          {/* Logo */}
-                          <div className="content-header-item">
-                              <Link className="link-effect font-w700" to="/">
-                                  <span className="text-dual-primary-dark">مدیریت هزینه ها</span>
-                              </Link>
-                          </div>
-                          {/* END Logo */}
-                      </div>
-                      {/* END Normal Mode */}
-                  </div>
-                  {/* END Side Header */}
-                  {/* Side Navigation */}
-                  <div className="content-side content-side-full">
-                      <ul className="nav-main">
-                          {
-                              sideNavigation?.map(item =>(
-                                  <li key={item.href}>
-                                      <NavLink to={item.href}>
-                                          <i className={item.icon} />
-                                          <span className="sidebar-mini-hide">{item.label}</span>
-                                      </NavLink>
-                                  </li>
-                              ))
-                          }
-                      </ul>
-                  </div>
-                  {/* END Side Navigation */}
-              </div>
+  return (
+    <nav id="sidebar">
+      {/* Sidebar Scroll Container */}
+      <div className="slimScrollDiv">
+        <div id="sidebar-scroll">
           {/* Sidebar Content */}
-          </div><div className="slimScrollBar" /><div className="slimScrollRail"  /></div>
-          {/* END Sidebar Scroll Container */}
-      </nav>
-  )
-}
+          <div className="sidebar-content">
+            {/* Side Header */}
+            <div className="content-header content-header-fullrow px-15">
+              {/* Normal Mode */}
+              <div className="content-header-section text-center align-parent sidebar-mini-hidden">
+                {/* Close Sidebar, Visible only on mobile screens */}
+                {/* Layout API, functionality initialized in Codebase() -> uiApiLayout() */}
+                <button
+                  onClick={() => sidebarShow(false)}
+                  type="button"
+                  className="btn btn-circle btn-dual-secondary d-lg-none align-v-l"
+                  data-toggle="layout"
+                  data-action="sidebar_close"
+                >
+                  <i className="fa fa-times text-danger" />
+                </button>
+                {/* END Close Sidebar */}
+                {/* Logo */}
+                <div className="content-header-item">
+                  <Link className="link-effect font-w700" to="/">
+                    <span className="text-dual-primary-dark">
+                      مدیریت هزینه ها
+                    </span>
+                  </Link>
+                </div>
+                {/* END Logo */}
+              </div>
+              {/* END Normal Mode */}
+            </div>
+            {/* END Side Header */}
+            {/* Side Navigation */}
+            <div className="content-side content-side-full">
+              <ul className="nav-main">
+                {sideNavigation?.map((item) => (
+                  <li key={item.href}>
+                    <NavLink to={item.href}>
+                      <i className={item.icon} />
+                      <span className="sidebar-mini-hide">{item.label}</span>
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* END Side Navigation */}
+          </div>
+          {/* Sidebar Content */}
+        </div>
+        <div className="slimScrollBar" />
+        <div className="slimScrollRail" />
+      </div>
+      {/* END Sidebar Scroll Container */}
+    </nav>
+  );
+};
 
 export default memo(Sidebar);
