f2661fdbde1c3c462e3dcb49bbcec6bab667f993
aminnateghi10 <aminnateghi10@gmail.com>
2023-12-20 14:43:55 +0330

    change backend url

--- src/helpers/callApi.ts
+++ src/helpers/callApi.ts
@@ -5,7 +5,7 @@ const {REACT_APP_API_URL} = process.env;
 const callApi = () => {
     const cookies = new Cookies();
     const axiosInstance = axios.create({
-        baseURL: "http://127.0.0.1:8000/api/v1",
+        baseURL: "api.asdfasdfasdf.cfd/api/v1",
         headers: {'Authorization': cookies.get('token')}
     })
     axiosInstance.interceptors.request.use(
